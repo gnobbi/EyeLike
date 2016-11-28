cdc63640e990d18ce4d88d180e6ec3be73db4caf
gnobbi <github@danielnicklaus.info>
2016-11-28 19:37:01 +0100

    initial CMake project finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <Window/Window.h>
+#include <Image/Image.h>
+#include <xml/xml.h>
 
-
-int main()
+int main(int argc, char *argv[])
 {
 	std::cout << "Starting EyeLikeApp" << std::endl;
+	Window w(argc, argv);
+	Image i;
+	Xml x;
 }
